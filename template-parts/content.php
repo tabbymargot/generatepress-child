2c9e655193a98a6763dc87f00c74e0599415724a
tabbymargot <carolinewood72@gmail.com>
2020-10-15 15:40:35 +0100

    end week 5 video 19

--- template-parts/content.php
+++ template-parts/content.php
@@ -17,89 +17,62 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<?php 
 			// Checks for a row called content:
 			if( have_rows('content') ): while ( have_rows('content') ) : the_row();
+
 			// If 'content' exists, check for a row layout called 'header', and if it exists, go through the data and echo it out:
-			if( get_row_layout() == 'header' ): ?>
+			if( get_row_layout() == 'header' ): 
 
-				<!-- Header wrapper -->
-				<div class="c-single-post-header c-single-post-header@m">
-
-					<!-- Header image -->
-					<div class="c-single-post-header-image c-single-post-header-image@m" style="
-						<?php 
-							//In the video he removes the conditional 'if' part of this code - I think this a mistake, as if there's no hero image in ACF the page doesn't load at all.
-							if( get_field('hero_image') ):
-							//Custom function
-							nice_background('hero_image');
-							endif; 
-						?>"
-					>
-					</div>
-
-					<!-- Header content -->
-					<div class="c-single-post-header-content c-single-post-header-content@m">
-						<!-- data shared from our hero -->
-						<div class="c-single-post-header-content-wrapper">
-							<p class="c-single-post-header-date">
-								<?php 
-									// convert date into format readable by PHP, and then convert it into a string
-									//custom function defined in functions.php and called here
-									nice_date(get_field('date'));
-								?>
-							</p>
-								<!-- standard wordpress data -->
-								<h1 class="c-single-post-header-title">
-									<?php the_title(); ?>
-								</h1>
-								
-								<p class="c-single-post-header-subhead">
-									<?php the_field('subhead'); ?>
-								</p>
-								<!-- specific to this component (ACF calls components 'layouts') -->
-								<p class="c-single-post-header-description">
-									<?php the_sub_field('header_intro'); ?>	
-								</p>
-						</div>	
-					</div>
-				</div>
+			get_template_part( 'template-parts/content-header', get_post_type() );
+		?>
 			
 			<?php 
 				//If there's no header, check for a 'text-block', and then echo out the data:
 				elseif( get_row_layout() == 'text_block' ): ?>
+	
+			<?php
+				get_template_part( 'template-parts/content-text', get_post_type() );
+			?>
 
-				<div class="c-single-post-textblock">
-					<?php the_sub_field('text_content'); ?>
+			<?php 
+				elseif ( get_row_layout() == 'gallery' ):
+			?>
+				
+			<?php 
+				$images = get_sub_field('gallery') 
+			?>
+
+			<?php foreach($images as $image) : ?>
+				<div class="c-gallery-image">
+					<?php 
+						// get each image by its ID and the size we want
+						echo wp_get_attachment_image($image['id'], 'full');
+					?>
+					<?php
+						//Set variable to hold caption
+						$caption = wp_get_attachment_caption($image['id']);
+					?>
+
+					<?php 
+						//Check to see if $caption is/isn't empty
+						if(!empty($caption)) :
+					?>
+
+						<p class="o-caption">
+							<?php 
+								// If it's not empty, echo $caption
+								echo $caption; 
+							?>
+						</p>
+						
+					<?php  endif; ?>	
 				</div>
-
-				<?php 
-					elseif ( get_row_layout() == 'gallery' ):
-				?>
-				<h1>Here's the gallery</h1>
-				<?php 
-					$images = get_sub_field('gallery') 
-				?>
-
-				<?php 
-					foreach($images as $image) {
-						print_r($image);
-					}
-				?>
+					
+			<?php endforeach; ?>
 
 			<?php endif; 
 		endwhile; endif; 
 		?>
 
-		<div class="c-gallery-image">
-			<?php 
-				// get an image by its ID and the size we want
-				echo wp_get_attachment_image(46, 'full');
-			?>
-			<p class="o-caption">
-				<?php 
-					// get image caption by id
-					echo wp_get_attachment_caption(46); 
-				?>
-			</p>	
-		</div>
+		
 		<!-- CUSTOM CODE ENDS HERE -->
 
 		<?php
